42a60873a990806745adcb922458c2c15a6fb258
= <=>
2024-10-20 18:55:22 -0400

    Handle stock availability and disable purchase button when out of stock

--- product_management.js
+++ product_management.js
@@ -1,3 +1,5 @@
+// Task 2: 
+
 const priceElement = document.getElementById('product-price')
 const colorSelector = document.getElementById('color-selector')
 
@@ -5,3 +7,21 @@ colorSelector.addEventListener("change", (event) => {
     const selectedPrice = event.target.value
     priceElement.textContent = `$${selectedPrice}`
 })
+
+// Task 3: 
+let stock = 12
+const inventoryCount = document.getElementById('inventory-count')
+const purchaseButton = document.getElementById('purchase-button')
+
+function updateInventory () {
+    if (stock > 0) {
+        purchaseButton.disable = false
+        inventoryCount.textContent = `In Stock: ${stock}`
+    }
+    else {
+        purchaseButton.disable = true
+        inventoryCount.textContent = 'Out of Stock'
+    }
+}
+
+purchaseButton.addEventListener
